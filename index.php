b99f4df00b475f55e2fd9a95a922a2ebd6af5a67
George McMullen <gmcmullen@mail.greenriver.edu>
2021-01-15 15:33:57 -0500

    Created index page

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Name: George McMullen & Patrick Dang
+ * Date: Jav 15 2021
+ * File URL:
+ * Project name: Pair Program 2
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+echo "PHP Array Practice.";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair Program 2</title>
+</head>
+<body>
+<h1>Pair Program 2</h1>
+</body>
+</html>
